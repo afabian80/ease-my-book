dcf48ce06fa25a5d959c512a4353bbce20997607
Akos Fabian <fabian.akos@gmail.com>
2016-07-29 17:42:56 +0200

    More features in haskell

--- Main.hs
+++ Main.hs
@@ -2,17 +2,57 @@ module Main where
 
 import Data.Char (isAlpha)
 import Data.Text (pack, unpack, breakOn)
+import Data.List.Split (splitOn)
 
 main :: IO ()
 main = do
         sampleHtml <- readFile "samples/VeryShortStories.htm"
         let (_, htmlBody) = breakOn (pack "<body") (pack sampleHtml)
-        let originalWords = collectWords (unpack htmlBody)
-        putStrLn $ unwords originalWords
+        let cleanBody = clean $ transform $ unpack htmlBody
+        let sentences = splitOn "\n" cleanBody
+        putStrLn $ unlines $ map (\x -> show (fst x) ++ ": " ++ snd x) (zip [1..] sentences)
+        --putStrLn (unlines $ map (\x -> show (fst x) ++ ": " ++ snd x) sentences)
+        -- let originalWords = collectWords (unpack htmlBody)
+        -- putStrLn $ unwords originalWords
 
-collectWords :: String -> [String]
-collectWords [] = []
-collectWords text@(x:xs)
-        | isAlpha x = takeWhile isAlpha text : collectWords (dropWhile isAlpha text)
-        | x == '<' = collectWords (dropWhile (/= '>') xs)
-        | otherwise = collectWords xs
+-- collectWords :: String -> [String]
+-- collectWords [] = []
+-- collectWords text@(x:xs)
+--         | isAlpha x = takeWhile isAlpha text : collectWords (dropWhile isAlpha text)
+--         | x == '<' = collectWords (dropWhile (/= '>') xs)
+--         | otherwise = collectWords xs
+
+transform :: String -> String
+transform [] = []
+transform text@(x:xs)
+        | isAlpha x = takeWhile isAlpha text ++ transform (dropWhile isAlpha text)
+        | isNewLine x = ' ' : transform xs
+        | isSentenceBorder x = x : '\n' : transform xs
+        | isSpecialChar x = x : transform xs
+        | x == '<' = transform (dropWhile (/= '>') xs)
+        | otherwise = transform xs
+
+clean :: String -> String
+clean [] = []
+clean (x:xs)
+        | isSpace x = ' ' : clean (dropWhile isSpace xs)
+        | isNewLine x = '\n' : clean (dropWhile isNewLine xs)
+        | otherwise = x : clean xs
+
+isSpace :: Char -> Bool
+isSpace c = c == ' '
+
+isNewLine :: Char -> Bool
+isNewLine c = c == '\n'
+
+isSentenceBorder :: Char -> Bool
+isSentenceBorder c = c `elem` ".;?!"
+
+isSpecialChar :: Char -> Bool
+isSpecialChar c = c `elem` ",;:- \n"
+
+-- TODO
+-- remove tags
+-- remove multiple linebreaks
+-- collapse multiple spaces
+-- break on [.?!] but also keep them in the text
