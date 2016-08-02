05e9f8ad193b18f7ef77aa8513178523c4781e47
Akos Fabian <fabian.akos@gmail.com>
2016-08-02 20:15:35 +0200

    Generate DB filenames

--- Database.hs
+++ Database.hs
@@ -1,8 +1,16 @@
-module Database (loadSingle) where
+module Database (loadSingle, generateFileNames) where
 
+import Text.Printf
 
-
-loadSingle :: FilePath -> IO ([String])
+loadSingle :: FilePath -> IO [String]
 loadSingle path = do
         fileText <- readFile path
         return (lines fileText)
+
+-- loadAll :: [IO [String]]
+-- loadAll =
+
+generateFileNames :: [String]
+generateFileNames = map (printf genPattern) ([1..25] :: [Integer])
+        where
+                genPattern = "frequency-lists/basewrd%02d.txt"
