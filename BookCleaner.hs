ac30ccae07c29d93d14885c326f79c6faa0ca56f
Akos Fabian <fabian.akos@gmail.com>
2016-08-02 08:04:32 +0200

    Extract to BookCleaner module

---
+++ BookCleaner.hs
@@ -0,0 +1,34 @@
+module BookCleaner (clean, transform) where
+
+import Data.Char (isAlpha)
+import Data.List (isPrefixOf)
+
+transform :: String -> String
+transform [] = []
+transform text@(x:xs)
+    | isAlpha x = takeWhile isAlpha text ++ transform (dropWhile isAlpha text)
+    | isNewLine x = ' ' : transform xs
+    | isSentenceBorder x = x : '\n' : transform xs
+    | isSpecialChar x = x : transform xs
+    | x == '<' = transform (dropWhile (/= '>') xs)
+    | "&nbsp;" `isPrefixOf` text = '\n' : transform (drop 6 text)
+    | otherwise = transform xs
+
+clean :: String -> String
+clean [] = []
+clean (x:xs)
+    | isSpace x = ' ' : clean (dropWhile isSpace xs)
+    | isNewLine x = '\n' : clean (dropWhile isNewLine xs)
+    | otherwise = x : clean xs
+
+isSpace :: Char -> Bool
+isSpace c = c == ' '
+
+isNewLine :: Char -> Bool
+isNewLine c = c == '\n'
+
+isSentenceBorder :: Char -> Bool
+isSentenceBorder c = c `elem` ".;?!"
+
+isSpecialChar :: Char -> Bool
+isSpecialChar c = c `elem` ",;:-'’ \n"
--- Main.hs
+++ Main.hs
@@ -1,10 +1,8 @@
 module Main where
 
-import Data.Char (isAlpha)
 import Data.Text (pack, unpack, breakOn)
 import Data.List.Split (splitOn)
-import Data.List (isPrefixOf)
-
+import BookCleaner (clean, transform)
 
 main :: IO ()
 main = do
@@ -21,33 +19,3 @@ main = do
 --         | isAlpha x = takeWhile isAlpha text : collectWords (dropWhile isAlpha text)
 --         | x == '<' = collectWords (dropWhile (/= '>') xs)
 --         | otherwise = collectWords xs
-
-transform :: String -> String
-transform [] = []
-transform text@(x:xs)
-        | isAlpha x = takeWhile isAlpha text ++ transform (dropWhile isAlpha text)
-        | isNewLine x = ' ' : transform xs
-        | isSentenceBorder x = x : '\n' : transform xs
-        | isSpecialChar x = x : transform xs
-        | x == '<' = transform (dropWhile (/= '>') xs)
-        | "&nbsp;" `isPrefixOf` text = '\n' : transform (drop 6 text)
-        | otherwise = transform xs
-
-clean :: String -> String
-clean [] = []
-clean (x:xs)
-        | isSpace x = ' ' : clean (dropWhile isSpace xs)
-        | isNewLine x = '\n' : clean (dropWhile isNewLine xs)
-        | otherwise = x : clean xs
-
-isSpace :: Char -> Bool
-isSpace c = c == ' '
-
-isNewLine :: Char -> Bool
-isNewLine c = c == '\n'
-
-isSentenceBorder :: Char -> Bool
-isSentenceBorder c = c `elem` ".;?!"
-
-isSpecialChar :: Char -> Bool
-isSpecialChar c = c `elem` ",;:-'’ \n"
