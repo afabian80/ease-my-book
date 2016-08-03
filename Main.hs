b43e8a51ace11580ef83e72817997aa2939010d3
Akos Fabian <fabian.akos@gmail.com>
2016-08-03 10:13:16 +0200

    Restructure logic from main method

--- Main.hs
+++ Main.hs
@@ -1,21 +1,19 @@
 module Main where
 
-import Data.Text (pack, unpack, breakOn)
-import Data.List.Split (splitOn)
-import BookCleaner (clean, transform)
 import Database (readDB)
-import TextProcessor (collectWords)
+import TextProcessor (collectWords, collectSentences)
 
 main :: IO ()
 main = do
         sampleHtml <- readFile "clash.html"
-        let (_, htmlBody) = breakOn (pack "<body") (pack sampleHtml)
-        let cleanBody = clean $ transform $ unpack htmlBody
-        let sentences = splitOn "\n" cleanBody
-        let cleanSentences = filter (/= ".") sentences
-        --putStrLn $ unlines $ map (\x -> show (fst x :: Integer) ++ ": " ++ snd x) (zip [1..100] cleanSentences)
+
+        let sentences = collectSentences sampleHtml
+        putStrLn $ unlines sentences
+
         --cocaDB <- readDB
         --putStrLn $ unlines (cocaDB !! 0)
-        let bookWords = collectWords sampleHtml
-        putStrLn $ unlines bookWords
+
+        -- let bookWords = collectWords sampleHtml
+        -- putStrLn $ unlines bookWords
+
         print "Done"
--- TextProcessor.hs
+++ TextProcessor.hs
@@ -1,6 +1,9 @@
-module TextProcessor (collectWords) where
+module TextProcessor (collectWords, collectSentences) where
 
 import Data.Char (isAlpha)
+import Data.Text (pack, unpack, breakOn)
+import BookCleaner (clean, transform)
+import Data.List.Split (splitOn)
 
 collectWords :: String -> [String]
 collectWords [] = []
@@ -8,3 +11,18 @@ collectWords text@(x:xs)
         | isAlpha x = takeWhile isAlpha text : collectWords (dropWhile isAlpha text)
         | x == '<' = collectWords (dropWhile (/= '>') xs)
         | otherwise = collectWords xs
+
+getHtmlBody :: String -> String
+getHtmlBody html = unpack $ snd $ breakOn (pack "<body") (pack html)
+
+cleanBody :: String -> String
+cleanBody body = clean $ transform body
+
+getSentences :: String -> [String]
+getSentences = splitOn "\n"
+
+cleanSentences :: [String] -> [String]
+cleanSentences = filter (/= ".")
+
+collectSentences :: String-> [String]
+collectSentences = cleanSentences . getSentences . cleanBody . getHtmlBody
