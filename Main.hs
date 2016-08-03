1c1806cd39722e3af440a4bdade24b6880597ded
Akos Fabian <fabian.akos@gmail.com>
2016-08-03 10:28:10 +0200

    Read input file from argument

--- Main.hs
+++ Main.hs
@@ -1,19 +1,32 @@
 module Main where
 
+import System.Environment (getArgs)
+import System.Exit (die)
 import Database (readDB)
 import TextProcessor (collectWords, collectSentences)
 
 main :: IO ()
 main = do
-        html <- readFile "clash.html"
+        args <- getArgs
+        case args of
+                [inputFile] -> run inputFile
+                _ -> die "Use parameter: <html-inpu-file>"
+
+
+run :: FilePath -> IO ()
+run inputFile = do
+        html <- readFile inputFile
 
         let originalSentences = collectSentences html
-        --putStrLn $ unlines originalSentences
+        let numSentences = length originalSentences
+        putStrLn $ "Number of sentences: " ++ show numSentences
 
         cocaDB <- readDB
+        let numDBs = length cocaDB
+        putStrLn $ "Number of DBs : " ++ show numDBs
         --putStrLn $ unlines (cocaDB !! 0)
 
         let originalWords = collectWords html
+        let numWords = length originalWords
+        putStrLn $ "Number of words: " ++ show numWords
         -- putStrLn $ unlines originalWords
-
-        print "Done"
