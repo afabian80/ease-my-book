93e11b1e21f483260bee7ad3c04135ab7062e152
Akos Fabian <fabian.akos@gmail.com>
2016-08-03 08:34:15 +0200

    Load all DB files

--- Database.hs
+++ Database.hs
@@ -7,10 +7,10 @@ loadSingle path = do
         fileText <- readFile path
         return (lines fileText)
 
--- loadAll :: [IO [String]]
--- loadAll =
-
-generateFileNames :: [String]
+generateFileNames :: [FilePath]
 generateFileNames = map (printf genPattern) ([1..25] :: [Integer])
         where
                 genPattern = "frequency-lists/basewrd%02d.txt"
+
+loadAll :: [IO [String]]
+loadAll = map (loadSingle) generateFileNames
