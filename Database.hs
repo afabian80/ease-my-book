d61da979f6180f7dab6d5f247d89d0472dc20e36
Akos Fabian <fabian.akos@gmail.com>
2016-08-04 20:02:54 +0200

    Change internal DB structure
    Use words in lists only, instead of list of complete word lines

--- Database.hs
+++ Database.hs
@@ -4,26 +4,26 @@ import           Data.List   (elemIndex)
 import qualified Data.Set    as Set
 import           Text.Printf
 
-loadSingle :: FilePath -> IO [String]
+loadSingle :: FilePath -> IO [[String]]
 loadSingle path = do
         fileText <- readFile path
-        return (lines fileText)
+        return (map words (lines fileText))
 
 generateFileNames :: [FilePath]
 generateFileNames = map (printf genPattern) ([1..25] :: [Integer])
         where
                 genPattern = "frequency-lists/basewrd%02d.txt"
 
-loadAll :: [IO [String]]
+loadAll :: [IO [[String]]]
 loadAll = map loadSingle generateFileNames
 
-readDB :: IO [[String]]
+readDB :: IO [[[String]]]
 readDB = sequence loadAll
 
-singleDBToSet :: [String] -> Set.Set String
-singleDBToSet db = Set.fromList $ words $ unwords db
+singleDBToSet :: [[String]] -> Set.Set String
+singleDBToSet db = Set.fromList $ words $ unwords $ concat db
 
-dbToSets :: [[String]] -> [Set.Set String]
+dbToSets :: [[[String]]] -> [Set.Set String]
 dbToSets = map singleDBToSet
 
 wordInSet :: String -> Set.Set String -> Bool
